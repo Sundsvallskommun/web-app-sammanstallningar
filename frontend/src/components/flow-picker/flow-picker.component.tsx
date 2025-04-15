4416f390521c2c7cb79b8682550f4c131a66449f
Emil Högberg <emil.hogberg@sundsvall.se>
2025-04-15 14:09:09 +0200

    feat: card colors are now randomized on initial render

--- frontend/src/components/flow-picker/flow-picker.component.tsx
+++ frontend/src/components/flow-picker/flow-picker.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useFlows } from '@services/flow-service/use-flows';
 import { Card, useSnackbar } from '@sk-web-gui/react';
 import { getFlow, useFlowStore } from '@services/flow-service/flow-service';
@@ -14,10 +14,16 @@ export const FlowPicker: React.FC<FlowPickerProps> = (props) => {
   const { handleChangeStep, currentStep } = props;
   const { t } = useTranslation();
   const toastMessage = useSnackbar();
-
+  const colors = ['vattjom', 'gronsta', 'bjornstigen', 'juniskar'] as const;
+  type CardColor = (typeof colors)[number];
   const { flows } = useFlows();
   const { setFlow } = useFlowStore();
   const { refresh: refreshSession } = useSession();
+
+  const randomizedColors: CardColor[] = useMemo(() => {
+    const shuffled = [...colors].sort(() => 0.5 - Math.random());
+    return (flows ?? []).map((_, i) => shuffled[i % shuffled.length]);
+  }, [flows]);
 
   const handleFlowPick = (id: string, version: number) => {
     if (id && version) {
@@ -47,9 +53,10 @@ export const FlowPicker: React.FC<FlowPickerProps> = (props) => {
 
       <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-24 flex-wrap ">
         {flows?.map((flow, index) => {
+          const color = randomizedColors[index];
           return (
             <Card
-              color="vattjom"
+              color={color}
               className="col-span-1 border-1 border-divider rounded-20"
               key={index}
               useHoverEffect
