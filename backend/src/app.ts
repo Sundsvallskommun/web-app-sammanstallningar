7ba7b865f118ca34e30abf8f86344bef58358e1f
Henrik Sandstrom <henrik.sandstrom@sundsvall.se>
2025-06-05 09:51:39 +0200

    Adjust profile

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -94,9 +94,13 @@ const samlStrategy = new Strategy(
 
     logger.info(`SAML Profile: ${JSON.stringify(profile)}`);
 
-    const { givenName, surname, citizenIdentifier, username } = profile;
+    const givenName = profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname'] ?? profile['givenName'];
+    const surname = profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname'] ?? profile['sn'];
+    const email = profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'] ?? profile['email'];
+    const groups = profile['http://schemas.xmlsoap.org/claims/Group']?.join(',') ?? profile['groups'];
+    const username = profile['urn:oid:0.9.2342.19200300.100.1.1'];
 
-    if (!givenName || !surname || !citizenIdentifier) {
+    if (!givenName || !surname || !email) {
       return done({
         name: 'SAML_MISSING_ATTRIBUTES',
         message: 'Missing profile attributes',
